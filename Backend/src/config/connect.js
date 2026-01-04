a3f13ceda56d50a8d363ae3b8033dcff7bcb970c
Adarsh01433 <adarshtiwarip1433@gmail.com>
2026-01-04 19:02:21 +0530

    Initial commit: frontend and backend

---
+++ Backend/src/config/connect.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+ export const connectDB = async(uri)=> {
+    try {
+        await mongoose.connect(uri)
+        console.log("DB Connected");
+        
+    } catch (error) {
+     console.log("DataBase connection error", error);
+     
+        
+    }
+}
