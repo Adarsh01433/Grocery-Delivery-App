c49afa13b7527d5981b75980083f5af95893f04f
Adarsh01433 <adarshtiwarip1433@gmail.com>
2026-01-06 20:22:16 +0530

    Add auth middleware

---
+++ Backend/src/routes/auth.js
@@ -0,0 +1,3 @@
+import {fetchUser, loginCustomer, loginDeliveryPartner, refreshToken} from "../controllers/auth/auth.js"
+import {updateUser} from "../controllers/tracking/user.js"
+import {verifyToken} from "../middleware/auth.js";
