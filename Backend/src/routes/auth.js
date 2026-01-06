fa1ca4d5d767a742393a1abf6014391ebe1ff327
Adarsh01433 <adarshtiwarip1433@gmail.com>
2026-01-06 20:40:28 +0530

    complete all routes

--- Backend/src/routes/auth.js
+++ Backend/src/routes/auth.js
@@ -3,7 +3,7 @@ import {updateUser} from "../controllers/tracking/user.js"
 import {verifyToken} from "../middleware/auth.js";
 
 
-export const authRoutes = async(req, reply)=> {
+export const authRoutes = async(fastify, options)=> {
 fastify.post("/customer/login", loginCustomer);
 fastify.post("/delivery/login", loginDeliveryPartner);
 fastify.post("/refresh-token", refreshToken);
