fa0ea913dd9cbcfbf6dcca2ecdc803238007eb69
Adarsh01433 <adarshtiwarip1433@gmail.com>
2026-01-07 21:01:19 +0530

    SplashScreen Added and added multiple screen

---
+++ Mobile/src/features/auth/SplashScreen.tsx
@@ -0,0 +1,44 @@
+import { Image, StyleSheet, Text, View } from 'react-native'
+import React, { FC, useEffect } from 'react'
+import { Colors } from '@utils/Constants'
+import Logo from "@assets/images/logo.jpeg"
+import { screenHeight, screenWidth } from '@utils/Scaling'
+import { navigate } from '@utils/NavigationUtils'
+
+
+const SplashScreen:FC = () => {
+   useEffect(()=> {
+    
+    const navigateUser = async()=> {
+      try {
+         navigate("CustomerLogin")
+      } catch (error) {
+        
+      }}
+    const timeoutId = setTimeout(navigateUser,1000)
+    return ()=>clearTimeout(timeoutId)
+   },[])
+
+  return (
+    <View>
+      <Text>SplashScreen</Text>
+      <Image  style = {styles.logoImage}source={Logo}/>
+    </View>
+  )
+}
+
+export default SplashScreen
+
+const styles = StyleSheet.create({
+   container :{
+    backgroundColor : Colors.primary,
+    flex : 1,
+    justifyContent : "center",
+    alignItems : "center"
+   },
+   logoImage : {
+    height : screenHeight * 0.7,
+    width : screenWidth * 0.7,
+    resizeMode : "contain"
+   }
+})
---
+++ Mobile/src/navigation/Navigation.tsx
@@ -0,0 +1,30 @@
+import React, { FC } from 'react'
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {NavigationContainer} from '@react-navigation/native'
+import { navigationRef } from '@utils/NavigationUtils';
+import SplashScreen from '@features/auth/SplashScreen';
+import DeliveryLogin from '@features/auth/DeliveryLogin';
+import CustomerLogin from '@features/auth/CustomerLogin';
+
+
+const Stack = createNativeStackNavigator()
+
+const Navigation:FC = () => {
+  return (
+    <NavigationContainer ref={navigationRef}>
+        <Stack.Navigator initialRouteName='SplashScreen' screenOptions={{headerShown : false}}>
+            <Stack.Screen name='SplashScreen' component={SplashScreen} options={{
+              animation : "fade"
+            }}/>
+
+              <Stack.Screen name='DeliveryLogin' component={DeliveryLogin} options={{
+                animation : "fade"
+              }}/>
+
+                <Stack.Screen name='CustomerLogin' component={CustomerLogin}/>
+        </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default Navigation
