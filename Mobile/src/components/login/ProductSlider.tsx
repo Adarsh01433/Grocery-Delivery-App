75ad4e8e91b6d4757cf24477da4fa90b7387b587
Adarsh01433 <adarshtiwarip1433@gmail.com>
2026-01-07 22:48:30 +0530

    Beautiful Product Slider completed

--- Mobile/src/components/login/ProductSlider.tsx
+++ Mobile/src/components/login/ProductSlider.tsx
@@ -1,6 +1,8 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useMemo } from 'react'
+import { Image, StyleSheet, Text, View } from 'react-native'
+import React, { FC, useMemo } from 'react'
 import { imageData } from '@utils/dummyData'
+import AutoScroll from "@homielab/react-native-auto-scroll"
+import { screenHeight, screenWidth } from '@utils/Scaling'
 
 const ProductSlider = () => {
 
@@ -15,13 +17,63 @@ const ProductSlider = () => {
   return (
     // koi touch nhi
     <View pointerEvents='none'>  
-      <Text>ProductSlider</Text>
+     <AutoScroll duration={10000} endPaddingWidth={0} style = {styles.autoScroll}>
+      <View style = {styles.gridContainer}>
+           {rows?.map((row:any, rowIndex:number)=> {
+            return(
+                <MemoizedRow key={rowIndex} row={row} rowIndex={rowIndex}/>
+            )
+           })}
+        </View>  
+     </AutoScroll>
     </View>
   )
 }
 
-export default ProductSlider
+const Row:FC<{row:typeof imageData; rowIndex:number}>=({row, rowIndex})=> {
+    return(
+        <View style = {styles.row}>
+            {row?.map((image, imageindex)=> {
+                const horizontalshift = rowIndex % 2===0 ? -18:18
+                return(
+                    <View key={imageindex} style = {[styles.itemContainer ,{transform : [{translateX : horizontalshift}]}]}>
+                    <Image source={image} style = {styles.image}/>
+                    </View>
+                )
+            })}
+        </View>
+    )
+}
+
+const MemoizedRow = React.memo(Row)
 
 const styles = StyleSheet.create({
-
+  autoScroll : {
+    position : "absolute",
+    zIndex : -2
+  },
+  gridContainer : {
+    justifyContent : "center",
+    overflow : 'visible',
+    alignItems : "center"
+  },
+  image : {
+    width : "100%",
+    height : "100%",
+    resizeMode : "contain"
+  },
+  row : {flexDirection : "row",
+    marginBottom : 10
+  },
+  itemContainer : {
+    marginBottom : 12,
+    marginHorizontal : 10,
+    width : screenWidth * 0.26,
+    height : screenWidth * 0.26,
+    backgroundColor : "#f2f2f2",
+       justifyContent : "center",
+       borderRadius : 25,
+       alignItems : "center"
+  }
 })
+export default ProductSlider
