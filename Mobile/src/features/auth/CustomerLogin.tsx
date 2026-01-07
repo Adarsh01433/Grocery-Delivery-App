0893ac183b17fa1449e9492a0d8cb107c5fcfbc1
Adarsh01433 <adarshtiwarip1433@gmail.com>
2026-01-07 22:56:59 +0530

    complete footer

--- Mobile/src/features/auth/CustomerLogin.tsx
+++ Mobile/src/features/auth/CustomerLogin.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 import {GestureHandlerRootView , PanGestureHandler, State} from "react-native-gesture-handler"
 import CustomSafeAreaView from '@components/global/CustomSafeAreaView'
 import ProductSlider from '@components/login/ProductSlider'
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Colors } from '@utils/Constants'
 
 const CustomerLogin = () => {
   return (
@@ -11,6 +13,10 @@ const CustomerLogin = () => {
       <CustomSafeAreaView>
         <ProductSlider/>
       </CustomSafeAreaView>
+
+      <View style = {styles.footer}>
+        <SafeAreaView />
+      </View>
     </View>
    </GestureHandlerRootView>
   ) 
@@ -21,5 +27,18 @@ export default  CustomerLogin
 const styles = StyleSheet.create({
        container : {
         flex : 1
+       },
+       footer : {
+        borderTopWidth : 0.8,
+        borderColor : Colors.border,
+        paddingBottom : 10,
+        zIndex : 22,
+        position : 'absolute',
+        bottom : 0,
+        justifyContent : "center",
+        alignItems : "center",
+        padding : 10,
+        backgroundColor : "#f8f9fc",
+        width : "100%"
        }
 })
