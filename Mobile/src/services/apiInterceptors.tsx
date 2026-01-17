772e1d6cfc68b78dedf969c825f26aa66b04a581
Adarsh01433 <adarshtiwarip1433@gmail.com>
2026-01-17 14:52:18 +0530

    crearting interceptor request and response

--- Mobile/src/services/apiInterceptors.tsx
+++ Mobile/src/services/apiInterceptors.tsx
@@ -1,8 +1,44 @@
 import axios from "axios";
 import { BASE_URL } from "./config";
+import { refresh_token } from "./authService";
+import { tokenStorage } from "@state/storage";
 
 export const appAxios = axios.create({
     baseURL : BASE_URL
 })
 
-appAxios.interceptors.request
+appAxios.interceptors.request.use(async config => {
+    const accessToken = tokenStorage.getString('accesstoken')
+    if(accessToken){
+        config.headers.Authorization = `Bearer ${accessToken}`
+    }
+    return config
+})
+
+
+appAxios.interceptors.response.use(
+    response => response,
+    async error => {
+        if(error.response && error.response.status === 401){
+            try {
+                const newAccessToken = await refresh_token()
+                if( newAccessToken){
+                    error.config.headers.Authorization = `Bearer ${newAccessToken}`
+                    return axios(error.config)
+                }
+                
+            } catch (error) {
+              console.log("Error Refreshing Token");
+                 
+            }
+        }
+
+        if(error.response && error.response.status != 401){
+            const errorMessage = error.response.data.message || 'Something went wrong'
+            console.log(errorMessage);
+            
+            
+        }
+    }
+    
+)
